3f8a86c452042b775a2fd9336f52d313b7824b56
Rajeev R Sharma <i.rarsh@gmail.com>
2024-12-15 11:09:47 +0530

    feat: add models api endpoint for dynamic models fetch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
     ai: true,
+    cache: true,
   },
 
   // https://devtools.nuxt.com
